b9dec5759ef61f9a05e40f8540ffc89448a7b27b
Khushi Kakde <khushikakdekk03@gmail.com>
2025-07-25 19:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+let inputBox = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = '';
+
+buttons.forEach(element => {
+    element.addEventListener('click',(b) => {
+        if (b.target.innerText == '=') {
+            string =String(eval(string));
+            inputBox.value = string;
+        }
+        else if (b.target.innerText=='AC') {
+            string = '';
+            inputBox.value = string;
+        }
+        else if (b.target.innertText == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            inputBox.value = string;
+        }
+        else if (b.target.id == 'plusMinus') {
+            try {
+            string = String(-eval(string));
+            } catch (err) {
+            string = 'Error';
+            }
+            inputBox.value = string;
+        }
+        else 
+            string += b.target.innerText;
+            inputBox.value = string;
+        }
+    });
+});
